a8787c9213cbb716ee4822bff9c60678f4b302b7
emeibech <emeibech@gmail.com>
2022-03-11 11:11:07 +0800

    Create html and javascript file and write pseudocode for the script

---
+++ script.js
@@ -0,0 +1,22 @@
+// greet player & show score history
+// ask player if they want to play or reset score history
+// {if type 'reset', reset history
+// if they type 'play', start the game
+// else show error and ask again}
+
+// start the game | best of 5 | round 1 of 5
+// prompt for player name and put in a variable
+// get input from player (rock, paper, scissor)
+// {if input is neither (rock, paper, scissor) show error and ask for input again
+// else put input in a variable}
+// randomize input (rock, paper, scissor) for computer and put it in another variable
+// call function that determines the winner
+// [function condition: rock beats scissor, beaten by paper
+// paper beats rock, beaten by scissor
+// scissor beats paper, beaten by rock]
+// {if player wins, add 1 point to player score
+// else add 1 point to computer score}
+// {if player score reaches 3 points, declare player the winner and add 1 point to player score history
+// if computer wins reaches 3 points, declare computer the winner and add 1 point to computer score history}
+// ask player if they want to play another game
+// {if yes, start the game, else go back to greet player & show score history}
