bb2bb0bbda173d4410db1c08f80805682fbc32d4
emeibech <emeibech@gmail.com>
2022-03-14 12:28:37 +0800

    Create function to randomize computer pick

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+//functions
+function random(min, max) {
+    return min + Math.random() * (max - min);
+  }
+
 // greet player & show score history
 let scoreHistory = 'No score history yet.'
-let playOrReset;
+
 for (playOrReset = 'reset'; playOrReset === 'reset';) {
 let greeting = `Greetings, player. Here is the Score History: ${scoreHistory}`;
 alert(greeting);
@@ -13,10 +18,12 @@ playOrReset = prompt('Type "play" to start a new game or "reset" to reset score 
      scoreHistory = 'History has been reset.';
      alert('Score History has been reset');
     }
+
     // if player types 'play', start the game
     else if (playOrReset === 'play') {
-     gameStart();
+     break
     }
+
     // else show error and ask again}
     else {
         alert('error');
@@ -26,18 +33,49 @@ playOrReset = prompt('Type "play" to start a new game or "reset" to reset score 
 }
 
 // start the game | best of 5 | round 1 of 5
-function gameStart () {
-    alert('Best of 5');
-    alert('Round 1 of 5');
-    // prompt for player name and put in a variable
-    let playerName = prompt('What is your Name?', '');
-}
+
+// prompt for player name and put in a variable
+let playerName = prompt('What is your Name?', '');
+alert('Best of 5');
+alert('Round 1 of 5');
 
 // get input from player (rock, paper, scissor)
-// {if input is neither (rock, paper, scissor) show error and ask for input again
-// else put input in a variable}
+for (playerPick = 'error'; playerPick === 'error';) {
+    playerPick = prompt('Pick rock, paper, or scissors and type it in the window prompt','');
+
+    if (playerPick === 'rock' || playerPick === 'paper' || playerPick === 'scissors') {
+        break;
+    }
+
+    // {if input is neither (rock, paper, scissor) show error and ask for input again
+    else {
+        alert('error');
+        playerPick = 'error';
+    }
+    
+}
+
 // randomize input (rock, paper, scissor) for computer and put it in another variable
+
+let computerRandom = random(0, 2);
+computerRandom = computerRandom.toFixed();
+
+switch (computerRandom) {
+    case "0":
+        alert('Computer picks rock');
+        computerRandom = 'rock';
+        break;
+    case "1":
+        alert('Computer picks paper');
+        computerRandom = 'paper';
+        break;
+    case "2":
+        alert('Computer pics scissors');
+        computerRandom = 'scissors';
+        break;
+}
 // call function that determines the winner
+
 // [function condition: rock beats scissor, beaten by paper
 // paper beats rock, beaten by scissor
 // scissor beats paper, beaten by rock]
