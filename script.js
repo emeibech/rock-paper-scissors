f87d35a949470c0e99dd7f542653ec81e1360793
emeibech <emeibech@gmail.com>
2022-03-15 13:10:34 +0800

    Create loops and make player input case insensitive
    The whole thing is functional now

--- script.js
+++ script.js
@@ -5,13 +5,21 @@ function random(min, max) {
 
 // greet player & show score history
 let scoreHistory = 'No score history yet.'
+let playerScore = 0;
+let computerScore = 0;
+
+// prompt for player name and put in a variable
+let playerName = prompt('What is your Name?', '');
+
+for (i = 0; i < 999;) {
 
 for (playOrReset = 'reset'; playOrReset === 'reset';) {
-let greeting = `Greetings, player. Here is the Score History: ${scoreHistory}`;
+let greeting = `Greetings, ${playerName}. Here is the Score History: ${scoreHistory}`;
 alert(greeting);
 
 // ask player if they want to play or reset score history
 playOrReset = prompt('Type "play" to start a new game or "reset" to reset score history.', '');
+playOrReset = playOrReset.toLowerCase();
 
 // {if player types 'reset', reset score history
     if (playOrReset === 'reset') {
@@ -32,16 +40,41 @@ playOrReset = prompt('Type "play" to start a new game or "reset" to reset score 
 
 }
 
-// start the game | best of 5 | round 1 of 5
-
-// prompt for player name and put in a variable
-let playerName = prompt('What is your Name?', '');
+// start the game | best of 5
 alert('Best of 5');
-alert('Round 1 of 5');
+for (playerWins = '0', computerWins = '0'; playerWins <= '2' && computerWins <= '2';) {
+    //determine round
+    switch (playerWins + '|' + computerWins) {
+        case '1|0':
+            alert(`${playerName}: 1 | Computer: 0`);
+            break;
+        case '0|1':
+            alert(`${playerName}: 0 | Computer: 1`);
+            break;
+        case '2|0':
+            alert(`${playerName}: 2 | Computer: 0`);
+            break;
+        case '0|2':
+            alert(`${playerName}: 0 | Computer: 2`);
+            break;
+        case '1|1':
+            alert(`${playerName}: 1 | Computer: 1`);
+            break;
+        case '2|1':
+            alert(`${playerName}: 2 | Computer: 1`);
+            break;
+        case '1|2':
+            alert(`${playerName}: 1 | Computer: 2`);
+            break;
+        case '2|2':
+            alert(`${playerName}: 2 | Computer: 2`);
+            break;
+    }
 
-// get input from player (rock, paper, scissor)
+// get input from player (rock, paper, scissors)
 for (playerPick = 'error'; playerPick === 'error';) {
     playerPick = prompt('Pick rock, paper, or scissors and type it in the window prompt','');
+    playerPick = playerPick.toLowerCase();
 
     if (playerPick === 'rock' || playerPick === 'paper' || playerPick === 'scissors') {
         break;
@@ -55,33 +88,84 @@ for (playerPick = 'error'; playerPick === 'error';) {
     
 }
 
-// randomize input (rock, paper, scissor) for computer and put it in another variable
+// randomize input (rock, paper, scissors) for computer and put it in another variable
+let computerPick = random(0, 2);
+computerPick = computerPick.toFixed();
 
-let computerRandom = random(0, 2);
-computerRandom = computerRandom.toFixed();
-
-switch (computerRandom) {
-    case "0":
+switch (computerPick) {
+    case '0':
         alert('Computer picks rock');
-        computerRandom = 'rock';
+        computerPick = 'rock';
         break;
-    case "1":
+    case '1':
         alert('Computer picks paper');
-        computerRandom = 'paper';
+        computerPick = 'paper';
         break;
-    case "2":
-        alert('Computer pics scissors');
-        computerRandom = 'scissors';
+    case '2':
+        alert('Computer picks scissors');
+        computerPick = 'scissors';
         break;
 }
-// call function that determines the winner
+// determine the winner for this round
+if (playerPick === 'rock') {
+    switch (computerPick) {
+        case 'rock':
+            alert('Draw');
+            break;
+        case 'paper':
+            alert('You lose');
+            computerWins = Number(computerWins) + 1;
+            break;
+        case 'scissors':
+            alert('You win')
+            playerWins = Number(playerWins) + 1;
+            break;
+    }
+}
 
-// [function condition: rock beats scissor, beaten by paper
-// paper beats rock, beaten by scissor
-// scissor beats paper, beaten by rock]
-// {if player wins, add 1 point to player score
-// else add 1 point to computer score}
-// {if player score reaches 3 points, declare player the winner and add 1 point to player score history
-// if computer wins reaches 3 points, declare computer the winner and add 1 point to computer score history}
-// ask player if they want to play another game
-// {if yes, start the game, else go back to greet player & show score history}
+else if (playerPick === 'paper') {
+    switch (computerPick) {
+        case 'rock':
+            alert('You win');
+            playerWins = Number(playerWins) + 1;
+            break;
+        case 'paper':
+            alert('Draw');
+            break;
+        case 'scissors':
+            alert('You lose');
+            computerWins = Number(computerWins) + 1;
+            break;
+    }
+}
+
+else if (playerPick === 'scissors') {
+    switch (computerPick) {
+        case 'rock':
+            alert('You lose');
+            computerWins = Number(computerWins) + 1;
+            break;
+        case 'paper':
+            alert('You win');
+            playerWins = Number(playerWins) + 1;
+            break;
+        case 'scissors':
+            alert ('Draw');
+            break;
+    }
+}
+}
+
+// {if player score reaches 3 points, declare player the winner and update score history
+if (playerWins === 3) {
+    alert(`${playerName} wins the game`);
+    playerScore = playerScore + 1;
+}
+
+else if (computerWins === 3) {
+    alert('Computer wins the game');
+    computerScore = computerScore + 1;
+}
+
+scoreHistory = `${playerName}: ${playerScore} | Computer: ${computerScore}`;
+}
